9591c33cc0991c642d52cdd925466112e232d2cc
Guilherme Oliveira Loiola <guilhermeoliveira.0210@gmail.com>
2023-07-04 20:27:17 -0300

    Add some style

--- src/Chat.js
+++ src/Chat.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { initializeApp } from 'firebase/app';
 import { collection, addDoc, getDocs, getFirestore } from 'firebase/firestore';
+import { ChatFeed, Message } from 'react-chat-ui';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAqb-fVEhDyWPEJdMmFq-fCmQ6LuxpG994",
@@ -8,8 +9,7 @@ const firebaseConfig = {
   projectId: "miau-database",
   storageBucket: "miau-database.appspot.com",
   messagingSenderId: "241980715406",
-  appId: "1:241980715406:web:a771069d73e8188235b4df",
-  measurementId: "G-KBTQK3YJWL"
+  appId: "1:241980715406:web:a771069d73e8188235b4df"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -45,17 +45,47 @@ const Chat = () => {
 
   return (
     <div>
-      <ul>
-        {messages.map((message, index) => (
-          <li key={index}>
-            <strong>{message.name}:</strong> {message.text}
-          </li>
-        ))}
-      </ul>
-      <input type="text" value={newMessage} onChange={handleInputChange} />
-      <button onClick={sendMessage}>Enviar</button>
+      <ChatFeed
+        messages={messages.map(message => new Message({ id: message.id, message: message.text, senderName: message.name }))}
+      />
+      <div style={styles.inputContainer}>
+        <input
+          type="text"
+          value={newMessage}
+          onChange={handleInputChange}
+          style={styles.input}
+          placeholder="Digite sua mensagem..."
+        />
+        <button onClick={sendMessage} style={styles.button}>
+          Enviar
+        </button>
+      </div>
     </div>
   );
+};
+
+const styles = {
+  inputContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    marginTop: '10px',
+  },
+  input: {
+    flex: 1,
+    padding: '8px',
+    marginRight: '10px',
+    borderRadius: '4px',
+    border: '1px solid #ccc',
+    outline: 'none',
+  },
+  button: {
+    padding: '8px 16px',
+    borderRadius: '4px',
+    background: 'blue',
+    color: 'white',
+    border: 'none',
+    cursor: 'pointer',
+  },
 };
 
 export default Chat;
